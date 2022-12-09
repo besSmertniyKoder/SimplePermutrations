e1855fdb178b78bc225f051c94fc5e07e025f5e0
Roman Sukhachev <rsukhachev@bostonsd.ru>
2022-12-09 06:19:48 +0300

    What should i write here?

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] arr = {3, 4, 6, 2, 1, 9, 5};
+
+        Main.permute(arr, 0);
+
+        permuteWithoutCollections(Arrays.stream(arr).mapToInt(Integer::intValue).toArray());
+    }
+
+    static void permute(Integer[] arrayBefore, int fistElement) {
+        List<Integer> array = Arrays.asList(arrayBefore);
+        for (int i = fistElement; i < array.size(); i++) {
+            Collections.swap(array, i, fistElement);
+            permute(arrayBefore, fistElement + 1);
+
+            Collections.swap(array, fistElement, i);
+        }
+        if (fistElement == array.size() - 1) {
+            System.out.println(Arrays.toString(array.toArray()));
+        }
+    }
+
+    //Ниже рекурсивное решение без использования коллекций
+    static void permuteWithoutCollections(int[] arr) {
+        int count = fuctorial(arr.length);
+        int max = arr.length - 1;
+        int shift = max;
+        int t;
+        while (count > 0) {
+            t = arr[shift];
+            arr[shift] = arr[shift - 1];
+            arr[shift - 1] = t;
+            System.out.println(Arrays.toString(arr));
+            count--;
+            if (shift < 2) {
+                shift = max;
+            } else {
+                shift--;
+            }
+        }
+    }
+
+    static int fuctorial(int n) {
+        return (n > 0) ? n * fuctorial(n - 1) : 1;
+    }
+
+}
